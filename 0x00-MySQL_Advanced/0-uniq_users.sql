339bfa118fb06edba4e380884f12cf160dee1893
Timmygee21 <saytimmy@gmail.com>
2023-08-10 02:24:28 +0000

    A SQL script that creates a table users

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- a SQL script that creates a table of users
+-- with id, email, name as attributes
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY (id)
+);
